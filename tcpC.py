b0ea47a802cb4c4a6705edd3715e443bec8e33da
Chen Jiang <orange8637@gmail.com>
2012-08-13 23:43:17 +0800

    tcp

--- tcpC.py
+++ tcpC.py
@@ -1,24 +1,16 @@
 #! /usr/bin/env python
 # encoding: utf-8
-
 from socket import *
 
-HOST = '192.168.1.105'
+HOST = '192.168.1.103'
 PORT = 21567
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
-
 tcpCliSock = socket(AF_INET, SOCK_STREAM)
 tcpCliSock.connect(ADDR)
-
 while True:
 	data = raw_input('>>> ')
-	if not data:
-		break
 	tcpCliSock.send(data)
 	data = tcpCliSock.recv(BUFSIZE)
-	if not data:
-		break
 	print data
-
 tcpCliSock.close()
--- tcpS.py
+++ tcpS.py
@@ -1,29 +1,21 @@
 #! /usr/bin/env python
 # encoding:utf-8
-
 from socket import *
 from time import ctime
 
-HOST = '192.168.1.103'
+HOST = ''
 PORT = 21567
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
-
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
-
 while True:
 	print 'waiting for connection...'
 	tcpCliSock, addr = tcpSerSock.accept()
 	print '...connected from:',addr
-
 	while True:
 		data = tcpCliSock.recv(BUFSIZE)
-		if not data:
-			break
-		tcpCliSock.send('[%s] %s' %(ctime(), data))
-
+		tcpCliSock.send('the server replid at %s %s' %(ctime(), data))
 	tcpCliSock.close()
-
-#tcpSerSock.close()
+tcpSerSock.close()

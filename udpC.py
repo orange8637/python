7ca6fe79009b35749dca94056b10200068af9496
Chen Jiang <orange8637@gmail.com>
2012-08-14 15:12:22 +0800

    udp

---
+++ udpC.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# encoding: utf-8
+from socket import *
+
+HOST = '192.168.1.105'
+PORT = 2222
+ADDR = (HOST, PORT)
+BUFSIZE = 1024
+udpCliSock = socket(AF_INET, SOCK_DGRAM)
+while 1:
+	data = raw_input('>> ')
+	udpCliSock.sendto(data, ADDR)
+	data, addr = udpCliSock.recvfrom(BUFSIZE)
+	print data
+udpCliSock.close()
+
---
+++ udpS.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# encoding: utf-8
+from socket import *
+from time import ctime
+
+HOST = ''
+PORT = 2222
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+udpSerSock = socket(AF_INET, SOCK_DGRAM)
+while True:
+	data, addr = udpSerSock.recvfrom(BUFSIZE)
+	udpSerSock.sendto('[%s] %s' %(ctime(), data), addr)
+	print 'sendto ...%s' %(addr)
+udpSerSock.close()
